ceb2f7e5cd76e240f035f1511a63d8ba79f697c4
Volodymyr <vladimir.myshko93@gmail.com>
2023-07-31 09:42:26 +0300

    add: onwarn

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        warn(warning)
+      }
+    }
+  }
 })
